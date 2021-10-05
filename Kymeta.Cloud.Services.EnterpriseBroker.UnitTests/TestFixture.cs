e78dfc8f76c691e6930152a4da2b6cfc9f011da6
Paul Kilanowski <pkilanowski@kymetacorp.com>
2021-10-04 18:17:29 -0700

    Add unit test project.

---
+++ Kymeta.Cloud.Services.EnterpriseBroker.UnitTests/TestFixture.cs
@@ -0,0 +1,22 @@
+﻿using Kymeta.Cloud.Services.EnterpriseBroker.HttpClients;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kymeta.Cloud.Services.EnterpriseBroker.UnitTests
+{
+    public class TestFixture : IDisposable
+    {
+        public Mock<IAccountsClient> AccountsClient = new Mock<IAccountsClient>();
+        public Mock<IUsersClient> UsersClient = new Mock<IUsersClient>();
+        public Mock<IOracleClient> OracleClient = new Mock<IOracleClient>();
+
+        public void Dispose()
+        {
+            // Cleanup here
+        }
+    }
+}
