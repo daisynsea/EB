7a5cfdccdd1c3b782e5edf2eb9aa9acacc8f4137
Gray Griswold <ggriswold@kymetacorp.com>
2022-02-28 09:37:36 -0800

    Restore "Mobile" property on SalesforceContactModel.

--- Kymeta.Cloud.Services.EnterpriseBroker/Models/Salesforce/SalesforceContactModel.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker/Models/Salesforce/SalesforceContactModel.cs
@@ -5,6 +5,7 @@
         public string? ParentAccountId { get; set; }
         public string? Name { get; set; }
         public string? Phone { get; set; }
+        public string? Mobile { get; set; }
         public string? Email { get; set; }
         public string? FirstName => Name?.Split(' ')?.FirstOrDefault();
         public string? LastName => Name?.Split(' ')?.LastOrDefault();
