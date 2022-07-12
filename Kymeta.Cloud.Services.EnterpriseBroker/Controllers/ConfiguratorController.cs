ecabecc4af7d7b8c2babe1a8dceab620def0f79c
Haiyal <hgajjar@kymetacorp.com>
2022-07-13 01:30:33 +0530

    CLDSRV-13713 : Update quote email titles

---
+++ Kymeta.Cloud.Services.EnterpriseBroker/Controllers/ConfiguratorController.cs
@@ -0,0 +1,38 @@
+﻿using Kymeta.Cloud.Services.EnterpriseBroker.Models.Configurator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Kymeta.Cloud.Services.EnterpriseBroker.Controllers;
+
+[ApiController]
+[ApiVersion("1")]
+[Produces("application/json")]
+[Route("api/v{version:apiVersion}/configurator")]
+[ExcludeFromCodeCoverage]
+public class ConfiguratorController : ControllerBase
+{
+    private readonly ILogger<ConfiguratorController> _logger;
+    private readonly IConfiguratorQuoteRequestService _quoteRequestService;
+
+    public ConfiguratorController(ILogger<ConfiguratorController> logger, IConfiguratorQuoteRequestService quoteRequestService)
+    {
+        _logger = logger;
+        _quoteRequestService = quoteRequestService;
+    }
+
+    [HttpPost("createquoterecord"), AllowAnonymous]
+    public async Task<ActionResult<string>> CreateQuoteOrderId([FromBody] QuoteRequestViewModel model)
+    {
+        try
+        {
+            var result = await _quoteRequestService.InsertQuoteRequest(model);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error processing address action due to an exception: {ex.Message}");
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
---
+++ Kymeta.Cloud.Services.EnterpriseBroker/Services/ConfiguratorQuoteRequestService.cs
@@ -0,0 +1,30 @@
+﻿using Kymeta.Cloud.Services.EnterpriseBroker.Models.Configurator;
+
+namespace Kymeta.Cloud.Services.EnterpriseBroker.Services
+{
+    public interface IConfiguratorQuoteRequestService
+    {
+        Task<string> InsertQuoteRequest(QuoteRequestViewModel model);
+    }
+
+    public class ConfiguratorQuoteRequestService : IConfiguratorQuoteRequestService
+    {
+        private readonly IQuotesRepository _quotesRepository;
+        public ConfiguratorQuoteRequestService(IQuotesRepository quotesRepository)
+        {
+            _quotesRepository = quotesRepository;
+        }
+
+        public async Task<string> InsertQuoteRequest(QuoteRequestViewModel model)
+        {
+            model.Id = Guid.NewGuid();
+            var documents = await _quotesRepository.GetQuoteRecords();
+            var latestOrderId = documents?.FirstOrDefault()?.OrderId ?? 10000;
+            model.OrderId = latestOrderId + 1;
+            model.CreatedOn = DateTime.UtcNow;
+            var document =await _quotesRepository.InsertQuoteRecord(model);
+            var orderId = document?.OrderId;
+            return Convert.ToString(orderId);
+        }
+    }
+}
