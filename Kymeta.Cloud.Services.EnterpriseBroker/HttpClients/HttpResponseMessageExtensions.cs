7fc39b7e3d0fb2a3e23f9541676807e2d7d75020
maja <mdjermonovic@kymetacorp.com>
2023-02-10 07:13:11 -0800

    BP-242: Expose new method on Oracle client. Implement it and return OracleResponse

---
+++ Kymeta.Cloud.Services.EnterpriseBroker/HttpClients/HttpResponseMessageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Kymeta.Cloud.Services.EnterpriseBroker.Models.Oracle.REST;
+
+namespace Kymeta.Cloud.Services.EnterpriseBroker.HttpClients
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static async Task<OracleResponse> ProcessResponseFromOracle(this HttpResponseMessage response, CancellationToken token)
+        {
+            string content = await response.Content.ReadAsStringAsync(token);
+
+            return new OracleResponse(response.StatusCode, response.ReasonPhrase, content);
+        }
+    }
+}
