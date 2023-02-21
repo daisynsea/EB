b26663b777a93176bb6d1a033357fbf594e59b5d
Radmila Sekulic <rsekulic@kymetacorp.com>
2023-02-21 07:25:09 -0800

    BP-242: Add fields to SalesForceOrder.cs. Work in progress.

--- Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/SalesForceOrder.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/SalesForceOrder.cs
@@ -3,7 +3,21 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
 {
     public class SalesforceOrder
     {
-        OrderProduct[] Products { get; set; } = Array.Empty<OrderProduct>();
+        public string? Id { get; set; }
+         public string? OrderNumber { get; set; }
+        public string? BusinessUnit { get; set; }
+        public string? OrderType { get; set; }
+        public string? AccountName { get; set; }
+        public string? PrimaryContact { get; set; }
+        public string? PreferredContactMethod { get; set; }
+        public DateOnly? PoDate { get; set; }
+        public string? PoNumber { get; set; }
+        public string? BillToName { get; set; }
+        public string? OracleAccountId { get; set; }
+        public string? ShipToName { get; set; }
+        public string? ShippingAddress { get; set; }
+        public string? SalesRepresentative { get; set; }
+
     }
 
     public class OrderProduct
