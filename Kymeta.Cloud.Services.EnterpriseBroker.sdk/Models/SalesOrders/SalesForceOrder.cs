29881034325e48a17d68197baedd969e283023f5
maja <mdjermonovic@kymetacorp.com>
2023-02-24 05:42:15 -0800

    BP-242: Add properties needed on Product level

--- Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/SalesForceOrder.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/SalesForceOrder.cs
@@ -6,7 +6,7 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
     public class SalesforceOrder
     {
         public string? Id { get; set; }
-         public string? OrderNumber { get; set; }
+        public string? OrderNumber { get; set; }
         public string? BusinessUnit { get; set; }
         public string? OrderType { get; set; }
         public string? AccountName { get; set; }
@@ -30,9 +30,32 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
 
     public class OrderProduct
     {
+        public string Id { get; set; }
         public string? OrderId { get; set; }
         public string? BillingFrequency { get; set; }
         public string? NumberOfBillingPeriods { get; set; }
         public string? LineType { get; set; }
+        public string? Product_Code__c { get; set; }
+        public string Product2Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public object OriginalOrderItemId { get; set; }
+        public double Quantity { get; set; }
+        public string SBQQ__BillingFrequency__c { get; set; }
+        public string? Number_of_Billing_Periods__c { get; set; }
+        public double UnitPrice { get; set; }
+        public string? NetPrice__c { get; set; }
+        public string? ShipToContactId { get; set; }
+        public string? Preferred_Contact_Method__c { get; set; }
+        public string? Ship_Date__c { get; set; }
+        public string? Shipping_Terms__c { get; set; }
+        public string? Shipping_Details__c { get; set; }
+        public string? Packing_Instructions__c { get; set; }
+        public string? BillToAddress__c { get; set; }
+        public string? BillToContactId { get; set; }
+        public string? SBQQ__PaymentTerm__c { get; set; }
+        public string? Sync_Instructions__c { get; set; }
+        public string? Approved__c { get; set; }
+        public string? OrderNumber { get; set; }
+        public string? Oracle_Invoice__c { get; set; }
     }
 }
