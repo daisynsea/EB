447051224e37affea7786fbb9bee35cf827623f1
maja <mdjermonovic@kymetacorp.com>
2023-02-24 08:47:02 -0800

    BP-242: Change query how to get products

--- Kymeta.Cloud.Services.EnterpriseBroker.sdk/Clients/SalesforceRestApi.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Clients/SalesforceRestApi.cs
@@ -24,12 +24,33 @@ public class SalesforceRestApi : ISalesforceRestApi
 
     public async Task<List<OrderProduct>> GetOrderProducts(string orderKey, CancellationToken cancellationToken)
     {
-        //var limitToUnsyncedOrders = " and NEO_Sync_to_Oracle__c=false";
-        var query = $"select FIELDS(ALL) from OrderItem where orderId='{orderKey}' LIMIT 200"; // limitToUnsynced should we here after it's provisioned on SF env
-
-        HttpResponseMessage response = await _client.GetAsync($"/services/data/v56.0/query?q={query}", cancellationToken);
+        HttpResponseMessage response = await _client.GetAsync($"/services/data/v56.0/query?q={CreateQueryToGetProducts(orderKey)}", cancellationToken);
         string content = await response.Content.ReadAsStringAsync(cancellationToken);
         var lines = JsonConvert.DeserializeObject<SalesforceOrder>(content);
         return lines.OrderProducts;
+    }
+
+    private string CreateQueryToGetProducts(string orderKey)
+    {
+        var filedsToGet = @"Id, OrderId, Product_Code__c, Product2Id, IsDeleted, OriginalOrderItemId, Quantity, SBQQ__BillingFrequency__c,
+UnitPrice, NetPrice__c, Ship_Date__c";
+        //Number_of_Billing_Periods__c - error
+        //ShipToContactId - error
+        //Preferred_Contact_Method__c - error
+        //Shipping_Terms__c -- error
+        //Shipping_Details__c - error
+        //BillToAddress__c - error
+        // to be tested:
+        //Packing_Instructions__c,
+        //BillToContactId,
+        //Preferred_Contact_Method__c,
+        //SBQQ__PaymentTerm__c,
+        //Sync_Instructions__c ,
+        //Approved__c,
+        //OrderNumber,
+        //Oracle_Invoice__c";
+        var limitToUnsyncedOrders = " and NEO_Sync_to_Oracle__c=false";
+        var query = $"select {filedsToGet} from OrderItem where orderId='{orderKey}' {limitToUnsyncedOrders}";
+        return query;
     }
 }
