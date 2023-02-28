40e9d271af45a344e804b6a0dc13fa7bf7ada460
Radmila Sekulic <rsekulic@kymetacorp.com>
2023-02-28 04:44:19 -0800

    BP2-242: Add mappings based on wondu response.

--- Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/SalesforceNeoApproveOrderModel.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/SalesforceNeoApproveOrderModel.cs
@@ -64,6 +64,10 @@ public record SalesforceNeoApproveOrderPayload
     public string? NEO_Oracle_Bill_to_Contact_ID__c { get; init; }
     public string? NEO_Bill_To_Name__c { get; init; }
 
+    public string? NEO_Payment_Term__c { get; set; }
+    public string? NEO_Currency__c { get; set; }
+    public DateTime? NEO_Requested_Ship_Date__c { get; set; }
+
     public bool IsValid()
     {
         return NEO_Id__c.IsNotEmpty();
@@ -78,7 +82,7 @@ public record SalesforceNeoApproveOrderPayload
             AccountName = NEO_Account_Name__c,
             BusinessUnit = null, // see about this
             OrderNumber = NEO_OrderNumbrer__c,
-            OrderType = NEO_Order_Type_Oracle_Sync__c,
+            OrderType = NEO_Order_Type_Oracle_Sync__c,  
             PoDate = NEO_PO_Date__c,
             PoNumber = NEO_PO_Number__c,
             PrimaryContact = NEO_Primary_Contract__c,
@@ -102,19 +106,19 @@ public record SalesforceNeoApproveOrderPayload
             OrderKey = $"OPS:{NEO_OrderNumbrer__c}",
             SourceTransactionSystem = "OPS",
             BusinessUnitName = NEO_Internal_Company__c,
-            BuyingPartyName = NEO_Bill_To_Name__c, //check event
+            BuyingPartyName = NEO_Bill_To_Name__c,
             TransactionType = NEO_Order_Type_Oracle_Sync__c,
             FreezePriceFlag = false,
             FreezeShippingChargeFlag = false,
             FreezeTaxFlag = false,
             SubmittedFlag = true,
             BuyingPartyContactNumber= NEO_Oracle_Bill_to_Contact_ID__c,
-            PaymentTerms=null,
-            BuyingPartyNumber=null,
-            RequestedShipDate=null,
-            RequestingBusinessUnitName=null,
+            PaymentTerms=NEO_Payment_Term__c,
+            BuyingPartyNumber= NEO_Oracle_Account_ID__c ,
+            RequestedShipDate=NEO_Requested_Ship_Date__c,
+            RequestingBusinessUnitName = NEO_Internal_Company__c,
             SourceTransactionRevisionNumber=null,
-            TransactionalCurrencyCode=null,
+            TransactionalCurrencyCode= NEO_Currency__c,
         };
     }
 }
