4c9381e95029e05d5a3a30a3545116075dc50ad1
Radmila Sekulic <rsekulic@kymetacorp.com>
2023-03-01 01:12:59 -0800

    BP2-242: Change update order response.

--- Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/GetOrderResponse.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/GetOrderResponse.cs
@@ -24,10 +24,10 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? SourceTransactionNumber { get; set; }
         public string? SourceTransactionSystem { get; set; }
         public string? SourceTransactionId { get; set; }
-        public long BusinessUnitId { get; set; }
+        public long? BusinessUnitId { get; set; }
         public string? BusinessUnitName { get; set; }
         public DateTime TransactionOn { get; set; }
-        public long BuyingPartyId { get; set; }
+        public long? BuyingPartyId { get; set; }
         public string? BuyingPartyName { get; set; }
         public string? BuyingPartyNumber { get; set; }
         public string? BuyingPartyPersonFirstName { get; set; }
@@ -35,7 +35,7 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? BuyingPartyPersonMiddleName { get; set; }
         public string? BuyingPartyPersonNameSuffix { get; set; }
         public string? BuyingPartyPersonTitle { get; set; }
-        public long BuyingPartyContactId { get; set; }
+        public long? BuyingPartyContactId { get; set; }
         public string? BuyingPartyContactName { get; set; }
         public string? BuyingPartyContactNumber { get; set; }
         public string? BuyingPartyContactFirstName { get; set; }
@@ -43,15 +43,15 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? BuyingPartyContactMiddleName { get; set; }
         public string? BuyingPartyContactNameSuffix { get; set; }
         public string? BuyingPartyContactTitle { get; set; }
-        public long PreferredSoldToContactPointId { get; set; }
+        public long? PreferredSoldToContactPointId { get; set; }
         public string? CustomerPONumber { get; set; }
         public string? TransactionTypeCode { get; set; }
         public string? TransactionType { get; set; }
-        public bool SubstituteAllowedFlag { get; set; }
+        public bool? SubstituteAllowedFlag { get; set; }
         public string? PackingInstructions { get; set; }
         public string? ShippingInstructions { get; set; }
-        public bool ShipsetFlag { get; set; }
-        public bool PartialShipAllowedFlag { get; set; }
+        public bool? ShipsetFlag { get; set; }
+        public bool? PartialShipAllowedFlag { get; set; }
         public DateTime? RequestedShipDate { get; set; }
         public DateTime? RequestedArrivalDate { get; set; }
         public DateTime? LatestAcceptableShipDate { get; set; }
@@ -71,13 +71,13 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? DemandClass { get; set; }
         public string? FreightTermsCode { get; set; }
         public string? FreightTerms { get; set; }
-        public long RequestedFulfillmentOrganizationId { get; set; }
+        public long? RequestedFulfillmentOrganizationId { get; set; }
         public string? RequestedFulfillmentOrganizationCode { get; set; }
         public string? RequestedFulfillmentOrganizationName { get; set; }
         public string? SupplierId { get; set; }
         public string? SupplierName { get; set; }
         public string? SupplierSiteId { get; set; }
-        public int PaymentTermsCode { get; set; }
+        public int? PaymentTermsCode { get; set; }
         public string? PaymentTerms { get; set; }
         public string? SalespersonId { get; set; }
         public string? Salesperson { get; set; }
@@ -90,9 +90,9 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? CurrencyConversionType { get; set; }
         public string? PricingSegmentCode { get; set; }
         public string? PricingSegment { get; set; }
-        public long PricingStrategyId { get; set; }
+        public long? PricingStrategyId { get; set; }
         public string? PricingStrategyName { get; set; }
-        public bool AllowCurrencyOverrideFlag { get; set; }
+        public bool? AllowCurrencyOverrideFlag { get; set; }
         public string? SegmentExplanationMessageName { get; set; }
         public string? PricingSegmentExplanation { get; set; }
         public string? StrategyExplanationMessageName { get; set; }
@@ -102,14 +102,14 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? Comments { get; set; }
         public string? StatusCode { get; set; }
         public string? Status { get; set; }
-        public bool OnHoldFlag { get; set; }
-        public bool CanceledFlag { get; set; }
+        public bool? OnHoldFlag { get; set; }
+        public bool? CanceledFlag { get; set; }
         public string? CancelReasonCode { get; set; }
         public string? CancelReason { get; set; }
         public DateTime? RequestedCancelDate { get; set; }
-        public long RequestingBusinessUnitId { get; set; }
+        public long? RequestingBusinessUnitId { get; set; }
         public string? RequestingBusinessUnitName { get; set; }
-        public long RequestingLegalEntityId { get; set; }
+        public long? RequestingLegalEntityId { get; set; }
         public string? RequestingLegalEntity { get; set; }
         public bool FreezePriceFlag { get; set; }
         public bool FreezeShippingChargeFlag { get; set; }
@@ -118,16 +118,16 @@ namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders
         public string? SubmittedBy { get; set; }
         public DateTime SubmittedDate { get; set; }
         public string? TransactionDocumentTypeCode { get; set; }
-        public bool PreCreditCheckedFlag { get; set; }
+        public bool? PreCreditCheckedFlag { get; set; }
         public string? RevisionSourceSystem { get; set; }
-        public int SourceTransactionRevisionNumber { get; set; }
+        public int? SourceTransactionRevisionNumber { get; set; }
         public string? TradeComplianceResultCode { get; set; }
         public string? TradeComplianceResult { get; set; }
         public string? LastUpdatedBy { get; set; }
         public DateTime LastUpdateDate { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime CreationDate { get; set; }
-        public bool OpenFlag { get; set; }
+        public bool? OpenFlag { get; set; }
         public string? OrigSystemDocumentReference { get; set; }
         public string? OrderKey { get; set; }
         public string? AppliedCurrencyName { get; set; }
