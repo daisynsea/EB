4038461fcca8daf0ec926fd8cfcce9c77d29bcea
Radmila Sekulic <rsekulic@kymetacorp.com>
2023-03-02 04:57:43 -0800

    BP2-242: Adding composite request model to sync with salesforce.

--- Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/OracleSalesOrderResponseModel.cs
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Models/SalesOrders/OracleSalesOrderResponseModel.cs
@@ -12,3 +12,31 @@ public record OracleSalesOrderResponseModel
     public string? IntegrationStatus { get; init; }
     public string? IntergrationError { get; init; }
 }
+
+
+public class OracleSalesforceSyncRequest
+{
+    public bool AlOrNone => true;
+    public bool CollateSubrequests => true;
+    public CompositeRequest[] CompositeRequest { get; set; }
+}
+
+public class CompositeRequest
+{
+    public string Method => "PATCH";
+    public string Url { get; set; }
+    public string ReferenceId { get; set; }
+    public OracleOrderStatusSync Body { get; set; }
+}
+
+public class OracleOrderStatusSync
+{
+    public string Oracle_SO__c { get; set; }
+    public string Oracle_Status__c { get; set; }
+    public string Oracle_Sync_Status__c { get; set; }
+    public string NEO_Oracle_Integration_Error__c { get; set; }
+    public string NEO_Oracle_Integration_Status__c { get; set; }
+    public string NEO_Oracle_Sales_Order_Id__c { get; set; }
+    public string NEO_Oracle_Fulfillment_Id__c { get; set; } // this is for product line order
+    public string NEO_Oracle_Sales_Order_Line_Id__c { get; set; } // this is for product line order
+}
