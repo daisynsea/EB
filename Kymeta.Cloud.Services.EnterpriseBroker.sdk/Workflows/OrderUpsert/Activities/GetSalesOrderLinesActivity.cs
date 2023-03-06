b3009c2b71a5b9e1de95f6d92494dfdc0d800950
maja <mdjermonovic@kymetacorp.com>
2023-03-06 09:41:30 -0800

    BP-242: Re-arrange files for Order orchestrations

---
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Workflows/OrderUpsert/Activities/GetSalesOrderLinesActivity.cs
@@ -0,0 +1,17 @@
+﻿using DurableTask.Core;
+using Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders;
+using Kymeta.Cloud.Services.EnterpriseBroker.sdk.Workflows.OrderUpsert.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Workflows.OrderUpsert.Activities;
+
+public class GetSalesOrderLinesActivity : TaskActivity<SalesforceNeoApproveOrderModel, SalesOrderModel>
+{
+    private readonly ILogger<GetSalesOrderLinesActivity> _logger;
+    public GetSalesOrderLinesActivity(ILogger<GetSalesOrderLinesActivity> logger) => _logger = logger;
+
+    protected override SalesOrderModel Execute(TaskContext context, SalesforceNeoApproveOrderModel input)
+    {
+        return new SalesOrderModel();
+    }
+}
---
+++ Kymeta.Cloud.Services.EnterpriseBroker.sdk/Workflows/OrderUpsert/Activities/UpdateOracleSalesOrderActivity.cs
@@ -0,0 +1,46 @@
+﻿using DurableTask.Core;
+using Kymeta.Cloud.Services.EnterpriseBroker.sdk.Clients;
+using Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models;
+using Kymeta.Cloud.Services.EnterpriseBroker.sdk.Models.SalesOrders;
+using Microsoft.Extensions.Logging;
+
+namespace Kymeta.Cloud.Services.EnterpriseBroker.sdk.Workflows.OrderUpsert.Activities;
+
+public class UpdateOracleSalesOrderActivity : AsyncTaskActivity<OracleUpdateOrder, OracleSalesOrderResponseModel<UpdateOrderResponse>>
+{
+    private readonly ILogger<UpdateOracleSalesOrderActivity> _logger;
+    private readonly IOracleRestClient _oracleRestClient;
+
+    public UpdateOracleSalesOrderActivity(ILogger<UpdateOracleSalesOrderActivity> logger, IOracleRestClient oracleRestClient)
+    {
+        _logger = logger;
+        _oracleRestClient = oracleRestClient;
+    }
+
+    protected override async Task<OracleSalesOrderResponseModel<UpdateOrderResponse>> ExecuteAsync(TaskContext context, OracleUpdateOrder input)
+    {
+        if (!input.IsValid())
+        {
+            throw new InvalidOperationException("Please provide valid sales order!");
+        }
+
+        OracleResponse<UpdateOrderResponse> orderResponse = await _oracleRestClient.UpdateOrder(input, default);
+        if (orderResponse.IsSuccessStatusCode())
+        {
+            return new OracleSalesOrderResponseModel<UpdateOrderResponse>
+            {
+                IntegrationStatus = IntegrationConstants.Success,
+                IntergrationError = IntegrationConstants.Clear,
+                ResponseModel = orderResponse.Payload
+            };
+        }
+
+        return new OracleSalesOrderResponseModel<UpdateOrderResponse>
+        {
+            IntegrationStatus = IntegrationConstants.Failure,
+            IntergrationError = orderResponse.ErrorMessage,
+            ResponseModel = null
+        };
+    }
+
+}
